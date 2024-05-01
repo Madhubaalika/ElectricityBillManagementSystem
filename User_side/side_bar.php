a13d67777fae41d180e4ba9fb8ed9b806a075e2e
Madhubaalika <madhubaalika@gmail.com>
2024-05-01 18:54:51 +0530

    folder users

---
+++ User_side/side_bar.php
@@ -0,0 +1,17 @@
+<div id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+        <li class="active">
+            <a href="dashboard.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+        </li>
+        <li>
+            <a href="bill.php"><i class="fa fa-fw fa-dollar"></i> Bills </a>
+        </li>
+        <li>
+            <a href="transaction.php"><i class="fa fa-fw fa-table"></i> Transactions </a>
+        </li>
+        <li>
+            <a href="complaints.php"><i class="fa fa-fw fa-info"></i> Complaints </a>
+        </li>
+    </ul>
+</div>
+
---
+++ User_side/subcomplaint.php
@@ -0,0 +1,15 @@
+    <?php 
+    include("../Includes/session.php");
+    include("../Includes/config.php");
+    $id=$_SESSION['uid'];
+    $comp=$_POST["complaint"];
+    $aid    = rand(1,2);
+    $stat   ="NOT PROCESSED"; 
+    if(isset($_POST["complaint"]) && !empty($_POST["complaint"]))
+    {
+        $query  = "INSERT INTO complaint(userid,adminid,complaint,status)";
+        $query .= " VALUES ({$id},{$aid},'{$comp}','NOT PROCESSED')";
+        mysqli_query($con,$query);  
+    }
+    header("Location:complaints.php");   
+    ?>
