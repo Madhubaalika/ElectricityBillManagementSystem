c7def44cd48d4a4c2b7966f5f2523a4d40e199b6
Mathumithaaa <132802290+Mathumithaaa@users.noreply.github.com>
2024-05-07 21:56:15 +0530

    Add files via upload

---
+++ admin/billamtcount.php
@@ -0,0 +1,9 @@
+<?php 
+
+include('../Includes/config.php');
+
+$result = mysqli_query($con, 'SELECT SUM(amount) AS value_sum FROM transaction WHERE status = "PROCESSED"'); 
+$row = mysqli_fetch_assoc($result); 
+echo '$'.$sum = $row['value_sum'];
+
+?>
